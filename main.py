3e3578d4a456c09f95b48e4f2cbf1709b8e86e36
Vishal <83269653+overlordvishal97@users.noreply.github.com>
2024-07-08 23:24:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import smtplib
 import datetime as dt
 import random
+import os
+from dotenv import load_dotenv, dotenv_values
 
+load_dotenv()
 
-my_email = "richardronith@gmail.com"
-password = "pkajnfssisqcaydi"
+my_email = os.getenv("my_email")
+password = os.getenv("password")
 
 #testing
 now = dt.datetime.now()
@@ -19,7 +22,7 @@ if weekday == 6:
         connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(from_addr=my_email,
-                            to_addrs="richardronith@yahoo.com",
+                            to_addrs=os.getenv("to_addrs"),
                             msg=f"subject:Quotes for Motivation\n\n{quote}")
 
 #this sends mail using my mail.
